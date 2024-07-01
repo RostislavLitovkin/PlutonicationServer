09b599f61ba07984fc46c776e8154441432ef82a
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2024-07-01 17:32:44 +0200

    hydration icon

--- plutonication/dapp_icons.py
+++ plutonication/dapp_icons.py
@@ -2,7 +2,7 @@ from flask import send_file
 from .extensions import app
 
 
-@app.route("/dapp/hydradx-icon")
-def hydradx_icon():
-    return send_file("static/dapp_icons/hydradx-icon.png", mimetype='image/png')
+@app.route("/dapp/hydration-icon")
+def hydration_icon():
+    return send_file("static/dapp_icons/hydration.svg", mimetype='image/svg')
 
