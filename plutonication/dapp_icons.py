cd807a16455908689149d927d90478e1493c1b20
Rostislav Litovkin <rostislavlitovkin@gmail.com>
2024-07-07 15:04:07 +0200

    Hydration icon updated

--- plutonication/dapp_icons.py
+++ plutonication/dapp_icons.py
@@ -4,5 +4,5 @@ from .extensions import app
 
 @app.route("/dapp/hydration-icon")
 def hydration_icon():
-    return send_file("static/dapp_icons/hydration.svg", mimetype='image/png')
+    return send_file("static/dapp_icons/hydration.png", mimetype='image/png')
 
